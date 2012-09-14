22cc9bac36e158c3595c22c97b891b1d8c163778
jiyinyiyong <jiyinyiyong@gmail.com>
2012-09-14 09:03:15 +0800

    change directiories, hope there`s no bugs

--- readme.txt
+++ readme.txt
@@ -1,12 +1,13 @@
 
-## CoffeeScript 中文资源
+CoffeeScript 资源
 
 把豆瓣小组里的东西整理出来了, 欢迎 folk 然后提交资源
 欢迎 folk repo 提供一些链接上来或者增加栏目和设计效果什么,
-弄得比较仓促, 内容都没核实过, 反正等修补啦
+写的比较随意, 等待 Folk 和提交
 页面以 gh-pages 静态网页在以下链接打开:
 http://coffee-js.github.com/coffeescript.cn/
 
 得益于 TJ 大神的 Jade 和 Stylus, 代码组织起来非常轻松
-在 Jade 中直接用了 Markdown, 复杂的编辑也免掉了
-修改时注意安装 jade, stylus, marked, coffee-script 这几个模块
+在 Jade 中直接用了 Markdown, 剩下是 CoffeeScript, Stylus
+调试时先 `npm install` 安装模块, 然后 `./watch.sh` 运行编译命令
+哪位能给设计个漂亮一点的样式么?
---
+++ watch.sh
@@ -0,0 +1,17 @@
+
+# echo '------> start watching'
+cd `dirname $0`
+
+./node_modules/jade/bin/jade -O page/ -P src/index.jade
+./node_modules/stylus/bin/stylus -o page/ src/s.styl
+./node_modules/coffee-script/bin/coffee -o page/ -bc src/h.coffee
+# doodle page/ &
+
+# read
+
+# pkill -f 'jade -O page/ -w src/index.jade'
+# pkill -f 'stylus -o page/ -w src/page.styl'
+# pkill -f 'coffee -o page/ -wb src/handle.coffee'
+# pkill -f 'doodle page/ server.coffee'
+
+# echo '------> stop watching'
